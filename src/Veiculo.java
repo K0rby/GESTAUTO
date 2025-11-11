1a7342f9aeb03f734743dcd0ca70d67258ae8a06
Jota <majinbuudoidao2@gmail.com>
2025-11-10 22:17:47 -0300

    Camada de dados

---
+++ src/Veiculo.java
@@ -0,0 +1,13 @@
+public class Veiculo {
+    String modelo;
+    String cor;
+    int valor;
+    String status; 
+
+    public Veiculo(String modelo, String cor, int valor, String status){
+        this.modelo = modelo;
+        this.cor = cor;
+        this.valor = valor;
+        this.status = status;
+    }
+}
